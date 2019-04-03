cd3e8883dddf1a9417e1fce9c45ed5b05fd77ea4
Sofiya Vasileva <sofiyavv98@gmail.com>
2019-04-03 14:26:13 +0300

    Working Client and Server

---
+++ MyTCPClient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTCPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter server name: ");
+            string server = Console.ReadLine();
+
+            Connect(server);
+        }
+
+        static void Connect(string server)
+        {
+            try
+            {
+                var port = 1020;
+                var client = new TcpClient(server, port);
+
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    string message = Console.ReadLine();
+                    if (message == "quit")
+                        break;
+
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+
+                    data = new Byte[256];
+
+                    var responseData = string.Empty;
+
+                    var bytes = stream.Read(data, 0, data.Length);
+                    responseData = Encoding.UTF8.GetString(data, 0, bytes);
+                }
+
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine($"ArgumentNullException: {e}");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketException: {e}");
+            }
+
+            Console.WriteLine("Press Enter to continue...");
+        }
+    }
+}
---
+++ MyTCPListener/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTCPListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+
+            try
+            {
+                var port = 1020;
+                IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(localAddress, port);
+                server.Start();
+
+                byte[] bytes = new byte[256];
+                string data = null;
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for connection...");
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = Encoding.UTF8.GetString(bytes, 0, i);
+                        Console.WriteLine(data);
+
+                        byte[] message = Encoding.UTF8.GetBytes(data);
+
+                        stream.Write(message, 0, message.Length);
+                    }
+
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketException: {e}");
+            }
+            finally
+            {
+                server.Stop();
+            }
+
+            Console.WriteLine("Hit enter to continue...");
+        }
+    }
+}
